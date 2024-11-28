926c57f81b08cf0e37939ce3bfa4ee1ac8615c95
Akshat Raj <akshatraj2607@gmail.com>
2024-11-28 17:54:19 +0530

    Added Recipe Image Endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,9 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,3 +37,9 @@ urlpatterns = [
 
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root = settings.MEDIA_ROOT,
+                          )
